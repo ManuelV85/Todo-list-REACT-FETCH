a218bb071d4cfc4714060b143cdc229bc9c300db
VIAN258 <angelmorales891@gmail.com>
2023-02-16 12:02:22 +0000

    agregando fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //include images into your bundle
 
 //create your first component
@@ -9,10 +9,39 @@ const Home = () => {
     const [newTask, setNewTask] = useState([])
 	// Se agregó el estado para el valor del input
 	const [taskValue, setTaskValue] = useState("");
-    const validate = (e) => {
+    const [tasks, setTasks] = useState([])
+    useEffect(() => {
+        const getTasks = async ()=>{
+            try {
+                const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/vian")
+                if(!response.ok) {
+                    throw new Error(response.status) 
+                }
+                const obj = await response.json()
+                console.log(obj)
+                setTasks(obj)
+            }
+            catch(error) {
+             console.log(error)
+            }
+        }
+        getTasks()
+    },[]) 
+    
+    const validate = async (e) => {
         if(e.key ==='Enter'){
 			// Se cambió el valor que se le da a la tarea por el valor del estado
+            const obj = [ ...tasks,{"label": e.target.value, "done": false}]
+            const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/vian", {
+                method: "PUT", body: JSON.stringify(obj), mode: "cors", 
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                  },
+            })
+            const res = await response.json()
             setNewTask(current => [...current, taskValue])
+            setTasks(current=> [...tasks,...obj])
             e.target.value = "";
 			setTaskValue("")
         }
@@ -33,8 +62,8 @@ const deleteTask = (index) => {
             <input  type="text" value={taskValue} className="form-control" placeholder="Enter task" onChange={(e) => setTaskValue(e.target.value)} onKeyDown={(e) => validate(e)}/>
 			<p></p>
 			<ul className="list-group">
-            {newTask && newTask?.map ((item, index) => {
-                return <li key={index} onClick ={() => deleteTask(index)}  className="list-group-item">{item} <button  type="button" className="btn-close" aria-label="Close"></button></li>
+            {tasks && tasks?.map ((item, index) => {
+                return <li key={index} onClick ={() => deleteTask(index)}  className="list-group-item">{item.label} <button  type="button" className="btn-close" aria-label="Close"></button></li>
             })
             }
 	     	</ul>
